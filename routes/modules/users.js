0a464f69b0d80f67d6b56492dddec4875bb389d0
yichennn <yichennn1010@gmail.com>
2022-12-14 00:02:16 +0800

    功能：新增基礎註冊功能

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user')
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
@@ -14,7 +16,31 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then(user => {
+      //帳號已註冊過
+      if(user) {
+        console.log('此email已被註冊過')
+        return res.render('register', { name, email })
+      }
+      //密碼與確認密碼不同
+      if(password !== confirmPassword) {
+        console.log('密碼與確認密碼不同')
+        return res.render('register', { name, email })
+      }
 
+      return User.create({ name, email, password })
+        .then(() => res.redirect('/'))
+        .catch((err) => {
+          console.log(err);
+          res.render('errorPage')
+        })
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render('errorPage')
+    })
 })
 
 
