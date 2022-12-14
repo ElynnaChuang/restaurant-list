328886dfc49857248d505ecc9239ef8dc54cee69
yichennn <yichennn1010@gmail.com>
2022-12-14 14:35:26 +0800

    功能：新增使用者驗證（瀏覽網頁內容時）

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,13 @@ const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
 
+//引入middleware
+const { authenticator } = require('../middleware/authentication')
+
 // 設定路徑
 router.use('/users', users)
-router.use('/restaurants', restaurants)
-router.use('/', home)
+router.use('/restaurants', authenticator, restaurants)
+router.use('/', authenticator, home)
 
 // 匯出總路由
 module.exports = router
