bc177ae59dfcdf27dd198911cc4ea15f3bb563e3
yichennn <yichennn1010@gmail.com>
2022-12-15 12:20:22 +0800

    ( standard code

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -11,19 +11,18 @@ const SEEDER_USER = {
 }
 // 連線成功（因為有for迴圈...，與mongoose.js裡不同，所以在mongoose.js先export，在這邊再繼續寫其他動作）
 db.once('open', () => {
-
   bcrypt
     .genSalt(10)
     .then(salt => bcrypt.hash(SEEDER_USER.password, salt))
     .then(hash => {
       const { name, email } = SEEDER_USER
-      User.create({ name, email, password: hash})
+      User.create({ name, email, password: hash })
         .then(user => {
           const userID = user._id
           Promise.all(Array.from({ length: restaurants.results.length }, (_, i) => {
-              const {name, name_en, category, image, location, phone, google_map, rating, description} = restaurants.results[i]
-              return Restaurant.create({userID, name, name_en, category, image, location, phone, google_map, rating, description})
-            }))
+            const { name, name_en, category, image, location, phone, google_map, rating, description } = restaurants.results[i]
+            return Restaurant.create({ userID, name, name_en, category, image, location, phone, google_map, rating, description })
+          }))
             .then(() => {
               console.log('done.')
               process.exit()
